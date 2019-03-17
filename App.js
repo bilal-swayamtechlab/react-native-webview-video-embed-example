403cd542d3349f0627de05f72beccdec068139f2
Joey Scarim <joeyscarim@gmail.com>
2019-03-16 20:29:32 -0600

    add frontend

--- App.js
+++ App.js
@@ -1,31 +1,88 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
 import React, { Component } from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  TouchableOpacity
+} from 'react-native';
+import WebView from 'react-native-android-fullscreen-webview-video';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu'
-});
-
-type Props = {};
-export default class App extends Component<Props> {
+export default class App extends Component {
+  state = {
+    playing: 0
+  };
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
           Welcome to React Native Webview example!
         </Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+
+        <View style={{ flex: 1, flexDirection: 'row' }}>
+          <TouchableOpacity
+            style={styles.loginScreenButton}
+            onPress={() => navigate('HomeScreen')}
+            underlayColor="#fff"
+          >
+            <Text style={styles.loginText}>YouTube</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.loginScreenButton}
+            onPress={() => navigate('HomeScreen')}
+            underlayColor="#fff"
+          >
+            <Text style={styles.loginText}>Vimeo</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.loginScreenButton}
+            onPress={() => navigate('HomeScreen')}
+            underlayColor="#fff"
+          >
+            <Text style={styles.loginText}>DailyMotion</Text>
+          </TouchableOpacity>
+        </View>
+
+        <View>
+          {/* <Text>Video area</Text> */}
+
+          <WebView
+            useWebKit={true}
+            source={{
+              uri: this.props.videoSource
+            }}
+            style={{
+              // marginVertical: 20,
+              // zIndex: -1,
+              // elevation: -1,
+              //   backgroundColor: this.state.webviewBackgroundColor,
+              flex: 1,
+              height: (width - 25) / 1.77777777778,
+              width: width - 25,
+              opacity: this.state.webviewBackgroundColor
+              //   backgroundColor: '#000'
+            }}
+            onLoadStart={() => {
+              this.setState({ webviewBackgroundColor: 0 });
+            }}
+            onLoadEnd={() => {
+              setTimeout(() => {
+                this.setState({ webviewBackgroundColor: 100 });
+              }, 1000);
+
+              //   this.opacity = 100;
+              //   this.setState(() => {
+              //     return {
+              //       webviewBackgroundColor: 100 // override container's backgroundColor
+              //     };
+            }}
+          />
+        </View>
+
+        <Text style={styles.instructions}>
+          Press a button above to play each video
+        </Text>
       </View>
     );
   }
@@ -36,16 +93,42 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF'
+    // backgroundColor: '#F5FCFF',
+    marginVertical: 100
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10
+  // welcome: {
+  //   fontSize: 20,
+  //   textAlign: 'center',
+  //   margin: 10
+  // },
+  // instructions: {
+  //   textAlign: 'center',
+  //   color: '#333333',
+  //   marginBottom: 5
+  // },
+  loginScreenButton: {
+    // marginRight: 40,
+    // marginLeft: 40,
+    // marginTop: 10,
+    // paddingTop: 10,
+    // paddingBottom: 10,
+    backgroundColor: 'blue',
+    // borderRadius: 10,
+    // borderWidth: 1,
+    // borderColor: '#fff',
+    // width: '100%'
+    alignSelf: 'stretch',
+    height: 60,
+    // textAlign: 'center',
+    margin: 10,
+    padding: 10,
+    borderRadius: 5
   },
-  instructions: {
+  loginText: {
+    color: '#fff',
     textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5
+    fontWeight: 'bold'
+    // paddingLeft: 10,
+    // paddingRight: 10
   }
 });
