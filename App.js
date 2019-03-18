66576fb84ad4f83e6c9b7318ea32b6de20fca361
Joey Scarim <joeyscarim@gmail.com>
2019-03-17 18:50:25 -0600

    add youtube and vimeo examples, cleanup styles

--- App.js
+++ App.js
@@ -1,28 +1,31 @@
 import React, { Component } from 'react';
 import {
-  Platform,
   StyleSheet,
   Text,
   View,
-  Button,
   TouchableOpacity,
   Dimensions,
   ScrollView,
   SafeAreaView
 } from 'react-native';
 import WebView from 'react-native-android-fullscreen-webview-video';
+
+// get the width of the current device
 const width = Dimensions.get('window').width;
 
 export default class App extends Component {
   state = {
-    playing: 0
+    uri: 'https://www.youtube.com/embed/O9IN4nAOhRM',
+    opacity: 0
   };
   render() {
     return (
       <SafeAreaView style={styles.container}>
         {/* headline */}
-        <Text style={styles.welcome}>Welcome to</Text>
-        <Text style={styles.welcome}>React Native Webview Example</Text>
+        <View style={{ marginVertical: 20 }}>
+          <Text style={styles.welcome}>Welcome to</Text>
+          <Text style={styles.welcome}>React Native Webview Example</Text>
+        </View>
 
         {/* video embed */}
         <ScrollView
@@ -37,35 +40,27 @@ export default class App extends Component {
           <WebView
             useWebKit={true}
             source={{
-              // uri: this.props.videoSource
-              uri: 'https://www.youtube.com/embed/_sZH-psg9yE'
+              uri: this.state.uri
             }}
             style={{
-              //   backgroundColor: this.state.webviewBackgroundColor,
               flex: 1,
               height: (width - 30) / 1.77777777778,
               width: width - 30,
-              opacity: this.state.webviewBackgroundColor
+              opacity: this.state.opacity
             }}
             onLoadStart={() => {
-              this.setState({ webviewBackgroundColor: 0 });
+              this.setState({ opacity: 0 });
             }}
             onLoadEnd={() => {
               setTimeout(() => {
-                this.setState({ webviewBackgroundColor: 100 });
-              }, 1000);
-
-              //   this.opacity = 100;
-              //   this.setState(() => {
-              //     return {
-              //       webviewBackgroundColor: 100 // override container's backgroundColor
-              //     };
+                this.setState({ opacity: 100 });
+              }, 500);
             }}
           />
         </ScrollView>
 
         {/* buttons */}
-        <View style={{ height: 100 }}>
+        <View style={{ height: 70 }}>
           <View
             style={{
               justifyContent: 'space-between',
@@ -74,34 +69,56 @@ export default class App extends Component {
             }}
           >
             <TouchableOpacity
-              style={styles.loginScreenButton}
-              onPress={() => navigate('HomeScreen')}
-              underlayColor="#fff"
+              style={[
+                styles.button,
+                this.state.uri.includes('you') && {
+                  backgroundColor: 'gray'
+                }
+              ]}
+              onPress={() =>
+                this.setState({
+                  uri: 'https://www.youtube.com/embed/O9IN4nAOhRM'
+                })
+              }
             >
-              <Text style={styles.loginText}>YouTube</Text>
+              <Text style={styles.buttonText}>YouTube</Text>
             </TouchableOpacity>
             <TouchableOpacity
-              style={styles.loginScreenButton}
-              onPress={() => navigate('HomeScreen')}
-              underlayColor="#fff"
+              style={[
+                styles.button,
+                this.state.uri.includes('vimeo') && {
+                  backgroundColor: 'gray'
+                }
+              ]}
+              onPress={() =>
+                this.setState({
+                  uri: 'https://player.vimeo.com/video/192511901'
+                })
+              }
             >
-              <Text style={styles.loginText}>Vimeo</Text>
+              <Text style={styles.buttonText}>Vimeo</Text>
             </TouchableOpacity>
             <TouchableOpacity
-              style={styles.loginScreenButton}
-              onPress={() => navigate('HomeScreen')}
-              underlayColor="#fff"
+              style={[
+                styles.button,
+                this.state.uri.includes('daily') && {
+                  backgroundColor: 'gray'
+                }
+              ]}
+              onPress={() =>
+                this.setState({
+                  uri: 'https://www.dailymotion.com/embed/video/x72cxzt'
+                })
+              }
             >
-              <Text style={styles.loginText}>DailyMotion</Text>
+              <Text style={styles.buttonText}>DailyMotion</Text>
             </TouchableOpacity>
           </View>
         </View>
 
         {/* instructions */}
         <View>
-          <Text style={styles.instructions}>
-            Press a button above to play each video
-          </Text>
+          <Text style={styles.instructions}>Choose a video source</Text>
         </View>
       </SafeAreaView>
     );
@@ -110,13 +127,13 @@ export default class App extends Component {
 
 const styles = StyleSheet.create({
   container: {
-    marginVertical: 100,
     margin: 15
   },
   welcome: {
     fontSize: 20,
     textAlign: 'center',
-    margin: 10
+    margin: 3,
+    fontWeight: 'bold'
   },
   instructions: {
     fontSize: 16,
@@ -124,16 +141,15 @@ const styles = StyleSheet.create({
     color: '#333333',
     margin: 10
   },
-  loginScreenButton: {
+  button: {
     backgroundColor: 'blue',
-
-    width: '30%',
-    height: 40,
-    marginVertical: 10,
+    width: '32%',
+    height: 38,
+    marginVertical: 20,
     padding: 10,
     borderRadius: 5
   },
-  loginText: {
+  buttonText: {
     color: '#fff',
     textAlign: 'center',
     fontWeight: 'bold'
